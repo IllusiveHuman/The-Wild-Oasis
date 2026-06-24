96dd06e790420e60fcc3adc994ce2b6ea0ef607d
IllusiveHuman <ilyapetrovich78@gmail.com>
2026-06-24 19:47:53 +0300

    chore:update keys

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = "https://bcljdhmwuhahlktfunlv.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbGpkaG13dWhhaGxrdGZ1bmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5OTQzOTcsImV4cCI6MjA2NzU3MDM5N30.kB4hvLDEE5GG1nLrKL7jTFceNjPBPhgweu3G7uS3r-I"
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+export const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
